06e9b0a7e5ca3d3c486da2161a74f2d0e325f419
ksampada23 <ksampada@purdue.edu>
2024-11-15 08:14:26 -0500

    Updated with ability to make google events

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,88 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import image from './img/background0.png';
-import image1 from './img/background1.png';
+import image1 from './img/background2.png';
 import Calendar from 'react-calendar';
-
+import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+import DateTimePicker from 'react-datetime-picker';
 
 function App() {
   const [backgroundImage, setBackgroundImage] = useState(false);
   const [view, setView] = useState(false);
   const [hidden, setDisplay] = useState(false);
 
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+
+  const [ eventName, setEventName ] = useState("");
+  const [ eventDescription, setEventDescription ] = useState("");
+
+  const session = useSession(); // tokens, when session exists we have a user
+  const supabase = useSupabaseClient(); // talk to supabase!
+  const { isLoading } = useSessionContext();
+
+  
+
+  if(isLoading) {
+    return <></>
+  }
+
+  async function googleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        scopes: 'https://www.googleapis.com/auth/calendar'
+      }
+    });
+    if(error) {
+      alert("Error logging in to Google provider with Supabase");
+      console.log(error);
+    }
+  }
+
+  async function signOut() {
+    await supabase.auth.signOut();
+  }
+
+  async function createCalendarEvent() {
+    console.log("Creating calendar event");
+    const event = {
+      'summary': eventName,
+      'description': eventDescription,
+      'start': {
+        'dateTime': start.toISOString(), // Date.toISOString() ->
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+      },
+      'end': {
+        'dateTime': end.toISOString(), // Date.toISOString() ->
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+      }
+    }
+    await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+      method: "POST",
+      headers: {
+        'Authorization':'Bearer ' + session.provider_token // Access token for google
+      },
+      body: JSON.stringify(event)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      alert("Event created, check your Google Calendar!");
+    });
+  }
+
+  console.log(session);
+  console.log(start);
+  console.log(end);
+  console.log(eventName);
+  console.log(eventDescription);
+
+
   return (
     <>
   
-      <div class="background">
+      <div className="background">
     
         <img
           style={{
@@ -26,6 +95,10 @@ function App() {
           src={backgroundImage ? image1 : image}
           alt=""
         />
+
+        {!hidden && (
+        <h1 className = "title">FUTURE TITLE NAME</h1>
+        )}
         
         {!hidden && view && (
         <Calendar className = "calendar" ></Calendar>
@@ -34,7 +107,7 @@ function App() {
 
         {!hidden && (
           <button
-            class="input"
+            className="input"
             onClick={() => {
               setBackgroundImage(true);
               setDisplay(true);
@@ -45,7 +118,7 @@ function App() {
         )}
 
         {!hidden && (
-          <button class="switch" onClick={() => {
+          <button className="switch" onClick={() => {
             setView(!view);
           }}>
             SWITCHVIEW
@@ -55,7 +128,7 @@ function App() {
 
         {hidden && (
           <button
-            class="exit"
+            className="exit"
             onClick={() => {
               setBackgroundImage(false);
               setDisplay(false);
@@ -66,10 +139,39 @@ function App() {
         )}
 
         {hidden && (
-          <button class="confirm" onClick={() => {}}>
+          <button className="confirm" onClick={() => {}}>
             CONFIRM
           </button>
         )}
+        
+        {hidden && (
+        <div className = "events">
+          {session ?
+            <>
+              <h2 className = "intro">Hello, {session.user.email} !</h2>
+              <p className = "text" >Please enter your reminder's start time</p>
+              <DateTimePicker onChange={setStart} value={start} />
+              <p className = "text" >Please enter your reminder's end time</p>
+              <DateTimePicker onChange={setEnd} value={end} />
+              <p className = "text" >Please enter your reminder's name</p>
+              <input type="text" onChange={(e) => setEventName(e.target.value)} />
+              <p className = "text" >Please enter your reminder's link</p>
+              <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
+              
+              <button className = "createEvent" onClick={() => createCalendarEvent()}>Create Calendar Event</button>
+         
+              <button className = "signOut" onClick={() => signOut()}>Sign Out</button>
+
+            
+            </>
+            :
+            <>
+              <button onClick={() => googleSignIn()}>Sign In With Google</button>
+            </>
+          }
+        </div>
+        )}
+
       </div>
        
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+"https://qnoescggtknwrtnvtbzj.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFub2VzY2dndGtud3J0bnZ0YnpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2NDMxMDQsImV4cCI6MjA0NzIxOTEwNH0.e2PBcxAUPSW1C3MnC1jT5nRCxAY4MwXtfn2V5j3W2VI"
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
     <App />
+    </SessionContextProvider>
   </StrictMode>
 );
